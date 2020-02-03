b8b9b2a11c2c0fd9c85e68f75cbe69f43b974198
Mathias <juniormathiias@gmail.com>
2020-02-03 16:46:34 +0000

    I added the regressive mode to count

--- script.js
+++ script.js
@@ -6,13 +6,23 @@ function counter(){
     if( from.value.length == 0 || to.value.length == 0 || steps.value.length == 0){
         alert("Fill up the Data")
     }else{
+        res.innerHTML = ``
         var f = Number(from.value)
         var t = Number(to.value)
         var s = Number(steps.value)
-        for( var c= f; c <= t; c = c + s){
-           res.innerHTML += `  ${c}` 
+        if( f < t){
+            // Increasing count
+             for( var c= f; c <= t; c = c + s){
+           res.innerHTML += `  ${c} \u{1f449}` 
+            }
+        }else {
+            // Regressive
+            for( var c= f; c>= t; c -= s){
+                res.innerHTML += `  ${c} \u{1f449}`
+            }
         }
-        
+       
+        res.innerHTML += `\u{1f3c1}`
     }
 
 
